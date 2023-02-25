3a8ca9bcdbce03fd3ebdf90f94ae447de2e31251
RobGro <r.grodzki74@gmail.com>
2023-02-25 20:45:02 +0000

    Initial commit

---
+++ src/main/java/dev/robgro/cleaningserviceapp/CleaningServiceAppApplication.java
@@ -0,0 +1,13 @@
+package dev.robgro.cleaningserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningServiceAppApplication.class, args);
+	}
+
+}
