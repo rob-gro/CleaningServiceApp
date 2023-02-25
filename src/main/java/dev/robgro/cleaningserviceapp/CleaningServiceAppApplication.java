8ebf1a2f04eee35f0811ff6296650f618180447a
RobGro <r.grodzki74@gmail.com>
2023-02-25 20:57:17 +0000

    Added Datasource configuration

--- src/main/java/dev/robgro/cleaningserviceapp/CleaningServiceAppApplication.java
+++ src/main/java/dev/robgro/cleaningserviceapp/CleaningServiceAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CleaningServiceAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CleaningServiceAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningServiceAppApplication.class, args);
+    }
 
 }
