cc95d783a493acb3787557db6076bcaffddb554e
RobGro <r.grodzki74@gmail.com>
2023-02-25 21:05:40 +0000

    Added Client and House entities

---
+++ src/main/java/dev/robgro/cleaningserviceapp/client/Client.java
@@ -0,0 +1,127 @@
+package dev.robgro.cleaningserviceapp.client;
+
+import dev.robgro.cleaningserviceapp.house.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @OneToMany(mappedBy = "client")
+    List<House> houses;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    public Client(List<House> houses, Long id, String firstName, String lastName, String phoneNumber, String email) {
+        this.houses = houses;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Client() {
+    }
+
+    public List<House> getHouses() {
+        return houses;
+    }
+
+    public void setHouses(List<House> houses) {
+        this.houses = houses;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (!Objects.equals(houses, client.houses)) return false;
+        if (!Objects.equals(id, client.id)) return false;
+        if (!firstName.equals(client.firstName)) return false;
+        if (!lastName.equals(client.lastName)) return false;
+        if (!phoneNumber.equals(client.phoneNumber)) return false;
+        return email.equals(client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = houses != null ? houses.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + phoneNumber.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/dev/robgro/cleaningserviceapp/house/House.java
@@ -0,0 +1,127 @@
+package dev.robgro.cleaningserviceapp.house;
+
+import dev.robgro.cleaningserviceapp.client.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "houses")
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(name = "street_number")
+    private String streetNumber;
+
+    @Column(name = "street_name")
+    private String streetName;
+
+    @Column(name = "post_code")
+    private String postCode;
+
+    @Column(name = "city")
+    private String city;
+
+    public House(Long id, String streetNumber, String streetName, String postCode, String city) {
+        this.id = id;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.postCode = postCode;
+        this.city = city;
+    }
+
+    public House() {
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "id=" + id +
+                ", client=" + client +
+                ", streetNumber='" + streetNumber + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        House house = (House) o;
+
+        if (!id.equals(house.id)) return false;
+        if (!client.equals(house.client)) return false;
+        if (!streetNumber.equals(house.streetNumber)) return false;
+        if (!streetName.equals(house.streetName)) return false;
+        if (!postCode.equals(house.postCode)) return false;
+        return city.equals(house.city);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + client.hashCode();
+        result = 31 * result + streetNumber.hashCode();
+        result = 31 * result + streetName.hashCode();
+        result = 31 * result + postCode.hashCode();
+        result = 31 * result + city.hashCode();
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
