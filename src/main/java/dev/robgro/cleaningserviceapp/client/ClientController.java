520c64d28a8a1aaa1fcf1b2ece1b1fb281507e6b
RobGro <r.grodzki74@gmail.com>
2023-04-26 17:04:46 +0100

    Added Client controller and CRUD features

---
+++ src/main/java/dev/robgro/cleaningserviceapp/client/ClientController.java
@@ -0,0 +1,42 @@
+package dev.robgro.cleaningserviceapp.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping({"/clients","/clients.html"})
+    public String clientsList(Model model) {
+        model.addAttribute("clientsList", clientService.getAllClients());
+        return "clients";
+    }
+
+    @PostMapping("/clients/save")
+    public String saveClient(@ModelAttribute("clients") Client client) {
+        clientService.saveClient(client);
+        return "redirect:/clients";
+    }
+
+    @GetMapping("/clients/{id}/edit")
+    public String editClient(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("client", clientService.getClientById(id));
+        return "client_edit_form";
+    }
+
+    @GetMapping("/clients/{id}/delete")
+    public String deleteClient(@PathVariable("id") Long id) {
+        clientService.deleteClientById(id);
+        return "redirect:/clients";
+    }
+}
