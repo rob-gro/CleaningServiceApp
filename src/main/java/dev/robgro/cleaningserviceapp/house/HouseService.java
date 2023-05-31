031cc55399cae5116031a469e86ffa0ec38b3b76
RobGro <r.grodzki74@gmail.com>
2023-05-31 21:43:11 +0100

    added House's service

---
+++ src/main/java/dev/robgro/cleaningserviceapp/house/HouseService.java
@@ -0,0 +1,16 @@
+package dev.robgro.cleaningserviceapp.house;
+
+import java.util.*;
+
+public interface HouseService {
+
+    List<House> getAllHouses();
+
+    House saveHouse(House house);
+
+    House getHouseById(Long id);
+
+    void deleteHouseById(Long id);
+
+    List<House> clientHousesList(Long client_id);
+}
