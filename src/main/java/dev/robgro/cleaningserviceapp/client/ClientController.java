0dce8f03231e96c9e0f8c748fa88e9f1cfa7864c
RobGro <r.grodzki74@gmail.com>
2023-06-07 17:32:06 +0100

    small cosmetic changes

--- src/main/java/dev/robgro/cleaningserviceapp/client/ClientController.java
+++ src/main/java/dev/robgro/cleaningserviceapp/client/ClientController.java
@@ -2,7 +2,10 @@ package dev.robgro.cleaningserviceapp.client;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClientController {
