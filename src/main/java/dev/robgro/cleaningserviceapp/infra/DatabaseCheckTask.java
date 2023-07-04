4ca04c6c9b0c8b67986c97d9b516c3d81caf406c
RobGro <r.grodzki74@gmail.com>
2023-07-04 21:33:28 +0100

    added reload data feature in case of deleted house entity or client entity

--- src/main/java/dev/robgro/cleaningserviceapp/infra/DatabaseCheckTask.java
+++ src/main/java/dev/robgro/cleaningserviceapp/infra/DatabaseCheckTask.java
@@ -1,6 +1,7 @@
 package dev.robgro.cleaningserviceapp.infra;
 
 import dev.robgro.cleaningserviceapp.repository.ClientRepository;
+import dev.robgro.cleaningserviceapp.repository.HouseRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,17 +9,20 @@ import org.springframework.stereotype.Component;
 public class DatabaseCheckTask {
 
     private final ClientRepository clientRepository;
+    private final HouseRepository houseRepository;
     private final DataLoader dataLoader;
 
-    public DatabaseCheckTask(ClientRepository clientRepository, DataLoader dataLoader) {
+    public DatabaseCheckTask(ClientRepository clientRepository, HouseRepository houseRepository, DataLoader dataLoader) {
         this.clientRepository = clientRepository;
+        this.houseRepository = houseRepository;
         this.dataLoader = dataLoader;
     }
 
-    @Scheduled(cron = "0 */30 * * * *")
+    @Scheduled(cron = "* */20 * * * *")
     public void checkAndInsertMissingRecords() {
-        long count = clientRepository.count();
-        if (count == 0) {
+        long countClients = clientRepository.count();
+        long countHouses = houseRepository.count();
+        if ((countClients == 0) || (countHouses == 0)) {
             dataLoader.loadInitialData();
         }
     }
