6adc3dfeb04e466ee973caf0f4ca7cc4a36c386f
RobGro <r.grodzki74@gmail.com>
2023-07-05 12:40:20 +0100

    fixed some bugs

--- src/main/java/dev/robgro/cleaningserviceapp/infra/DatabaseCheckTask.java
+++ src/main/java/dev/robgro/cleaningserviceapp/infra/DatabaseCheckTask.java
@@ -18,7 +18,7 @@ public class DatabaseCheckTask {
         this.dataLoader = dataLoader;
     }
 
-    @Scheduled(cron = "* */20 * * * *")
+    @Scheduled(cron = "* */30 * * * *")
     public void checkAndInsertMissingRecords() {
         long countClients = clientRepository.count();
         long countHouses = houseRepository.count();
